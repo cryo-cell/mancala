ab44546ec555d7422b0cf86f97e29b7e08aeaa92
cryo-cell <cryobabbi@gmail.com>
2024-06-02 00:45:58 -0400

    update styles

---
+++ js/home.js
@@ -0,0 +1,59 @@
+let homeArr =['M','A','N','C','A','L','A','M','A','N','C','A','L','A']
+
+function createHomePits(){
+
+       
+    let rightSide = document.getElementById("homeRight")
+    let leftSide = document.getElementById("homeLeft")
+    /*--CREATE SECOND PLAYER'S STORE--*/
+    
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+
+    async function load () { // We need to wrap the loop into an async function for this to work
+
+    for(let i = 0;i <homeArr.length; i++){
+            if(i==0||i<7){
+
+              /*--CREATE SECOND PLAYER'S STORE detailed--*/
+              let storeElement = document.createElement("div");
+              //create a div or button element to store the newly created Store class data 
+              storeElement.classList.add("store", "pit", "col"+0)
+              //add classnames to the element signifying store functionality
+              
+              let storeElementText = document.createTextNode(homeArr[i])
+              storeElement.append(storeElementText)
+              //append the value of store in the array as the text of the element
+      
+              //storeElement.setAttribute("id", arr[i].id)
+              //add a unique id to the element using the pit array
+              await timer(450);
+                    leftSide.append(storeElement)
+
+            
+              //append the Store class div element to the second player's container element
+          
+            
+            }
+            else{
+                    let pit = document.createElement("div");
+                                    let text = document.createTextNode(homeArr[i])
+                                    pit.classList.add("pit", "col"+i)
+                                    pit.appendChild(text)
+                                    //pit.setAttribute("id", `${i}`)
+                                    //create a new html button element using the pit class in the array
+                                    await timer(250);
+
+                                    rightSide.append(pit)
+                                    //griddy.append(pit)
+
+            }
+    }
+
+}
+
+load()
+
+}
+
+
+createHomePits()
--- js/index.js
+++ js/index.js
@@ -1,164 +1,212 @@
-//create a pit class that tracks amount and and id
+//create a pit class that tracks amount and an id
 class Pit {
         constructor(count, id){
-            this.count = 4;
+            this.count = count;
             this.id = id;
-    
         }
 }
-//do the same for Stores
+
+//create a store class that tracks amount and an id
 class Store {
         constructor(count, id){
-            this.count = 0;
+            this.count = count;
             this.id = id;
-    
         }
 }
 
-let arr =[]
-//create an array to keep track of each created pit's amount
+//create a game array of pits and stores.. we will use this array to create our html elements
+let arr = []
 
-let leftSide = document.getElementById("left")
-//store the left side container element variable
+function initializeGame(){
+//from 0 to 13, if i = 0 or i = 7 push a store to the arr
+//else push a pit to the arr
 
+        for(let i=0; i<14; i++){
+                if(i==0||i==7){
+                        let store = new Store(0, i);
+                        //create a new Store class 
+                        arr.push(store)
+                        //push the store into the array     
+                }
+                else {
+                        let pit = new Pit(4, i);
+                        //create a new Pit class 
 
-let leftStoreClass = new Store();
-//create a new Store class 
-//leftStoreClass.count = 0;
-
-leftStoreClass.id = 0;
-//this will be player number two's Score
-
-arr.push(leftStoreClass)
-//place the store into the array and index 0
-
-
-let leftStore = document.createElement("div");
-//create an element to store the newly created Store class data 
-
-leftStore.classList.add("store")
-leftStore.classList.add("pit")
-//add classnames to the element
-
-let leftStoreText = document.createTextNode(arr[0].count)
-leftStore.append(leftStoreText)
-//add the value of store in the array as the text of the element
-
-leftStore.setAttribute("id", arr[0].count)
-//add a unique id to the element using the pit array
-leftSide.append(leftStore)
-//append the Store class element to the left side container element
-
-/*Next we will:
-create 6 Pit class objects with id's ascending from 1 to 6 and counts of 4, 
-add the classes to the array, 
-create new buttons,
-and append the data to the buttons
-and append the buttons to the left side element*/
-
-for(let i=1; i<7; i++){
-        //for each loop from 1 to 6
-        let p = new Pit();
-        //create a new Pit each time
-        p.id = i;
-        //increase the id
-        //p.count = 4;
-        //set count to 4
-        arr.push(p);
-        //add the Pit to the array starting from index 1
-
-        let pit = document.createElement("button");
-        let text = document.createTextNode(arr[i].count)
-        pit.classList.add("pit")
-        pit.appendChild(text)
-        pit.setAttribute("id", `${i}`)
-        //create a new button element using the pit class in the array
-
-        leftSide.append(pit)
-        //append the pit element to the left side container
-
+                        /*set the id equal to i (1,2,3,4,5,6,8,9,10,11,12,13||(not equal to 0 or 7))*/
+                        arr.push(pit)
+                        //place the pit into the array
+                }
+        }
+        //console.log(arr)
 }
+initializeGame()
+/*function initializeGame(){}*/
 
-let rightSide = document.getElementById("right")
-let rightStoreClass = new Store();
-rightStoreClass.count = 0;
-rightStoreClass.id = 7;
-arr.push(rightStoreClass)
-let rightStore = document.createElement("div");
-rightStore.classList.add("store")
-rightStore.classList.add("pit")
-let rightStoreText = document.createTextNode(rightStoreClass.count)
-rightStore.append(rightStoreText)
-rightStore.setAttribute("id",7)
-rightSide.append(rightStore)
+function createPits(){
+        let rightSide = document.getElementById("right")
+        let leftSide = document.getElementById("left")
 
-for(let i=8; i<14; i++){
-        let p = new Pit();
-        p.id = i;
-        p.count = 4;
-        arr.push(p);
+        //FOR THE LENGTH OF ARR
+        //IF ARR[I].ID <7
+                //IF ARR[I].COUNT == 0 
+                        //APPEND A STORE TO LEFTSIDE
+                //IF NOT APPEND A PIT
+        //IF ARR[I].ID>7&&ARR[I].ID<14
+                //IF ARR[I].ID == 7
+                        //APPEND A STORE TO RIGHTSIDE
+                //IF NOT APPEND A PIT
+        
+        for(let i = 0;i <arr.length; i++){
+                if(arr[i].id < 7){
+                        if(arr[i].count == 0){
+                                /*--CREATE SECOND PLAYER'S STORE detailed--*/
+                                let storeElement = document.createElement("div");
+                                //create a div or button element to store the newly created Store class data 
+                                storeElement.classList.add("store", "pit", "col"+0)
+                                //add classnames to the element signifying store functionality
+                                
+                                let storeElementText = document.createTextNode(arr[i].count)
+                                storeElement.append(storeElementText)
+                                //append the value of store in the array as the text of the element
+                        
+                                storeElement.setAttribute("id", arr[i].id)
+                                //add a unique id to the element using the pit array
+                                leftSide.append(storeElement)
+                                //append the Store class div element to the second player's container element
+                        }
+                        else { 
+                                 /*--CREATE FIRST PLAYER'S PITS--*/
+                                        let pit = document.createElement("div");
+                                        let text = document.createTextNode(arr[i].count)
+                                        pit.classList.add("pit", "col"+i)
+                                        pit.appendChild(text)
+                                        pit.setAttribute("id", `${i}`)
+                                        //create a new html button element using the pit class in the array
 
-        let pit = document.createElement("button");
-        let text = document.createTextNode(p.count)
-        pit.classList.add("pit")
-        pit.appendChild(text)
-        pit.setAttribute("id", `${i}`)
+                                        leftSide.append(pit)
+                                        //griddy.append(pit)
 
-        rightSide.append(pit)
+                                        //append the pit button element to the left side container
+
+                                }
+                                //console.log(leftSide)
+        //let griddy2 = document.getElementById("container2")        
+
+                }
+                if(arr[i].id > 6 && arr[i].id<14){
+                        if(arr[i].count == 0){
+                                let rightStore = document.createElement("div");
+                                rightStore.classList.add("store", "pit", "col"+7)
+                                let rightStoreText = document.createTextNode(arr[i].count)
+                                rightStore.append(rightStoreText)
+                                rightStore.setAttribute("id", arr[i].id)
+                        
+                                rightSide.append(rightStore)
+                        
+                        }
+                        else {
+                                  /*--CREATE SECOND PLAYER'S PITS--*/
+                                        let pit = document.createElement("div");
+                                        let text = document.createTextNode(arr[i].count)
+                                        pit.classList.add("pit", "col"+i)
+                                        pit.appendChild(text)
+                                        pit.setAttribute("id", `${i}`)
+
+                                        rightSide.append(pit)
+                                        //griddy2.append(pit)
+      
+                        }
+
+                        
+                }
+
+
+        }
 }
+        /*STORE THE SECOND PLAYER'S HTML CONTAINER*/
+        //let leftSide = document.getElementById("left")
+        //store the left side container element in variable leftSide
+
+createPits()
+ 
+
+        /*Next we will:
+        create 6 Pit class objects with id's ascending from 1 to 6 and counts of 4, 
+        add the classes to the array, 
+        create new buttons,
+        and append the data to the buttons
+        and append the buttons to the second player's side element*/
+
+       /*--CREATE FIRST PLAYER'S STORE 
+                for more details see --CREATE SECOND PLAYER'S STORE detailed-- above*/
+                //let rightSide = document.getElementById("right")
+
+                //let rightStoreClass = new Store(0, 7);
+                //arr.push(rightStoreClass)
+        
+                
+                //griddy2.append(rightStore)
+
+      
+     
+/*function createPits(){}*/
+
 
 let pits = document.getElementsByClassName("pit")
-let count =0
+let count = 0
 let lastMoveAmount = []
 
-for(let i=8;i<14;i++){
-        document.getElementById(`${i}`).disabled = true;
-
-        //let aye = document.getElementById(`pit${i}`)
+function randomFirstPlayer(){
+Math.random() < 0.5 ? secondPlayerTurn() : firstPlayerTurn();
+//randomize who gets to go first
 }
-
 function secondPlayerTurn(){
-
-for(let i=1;i<7;i++){
-        document.getElementById(`${i}`).disabled = true;
-
-        //let aye = document.getElementById(`pit${i}`)
-}
-for(let i=8;i<14;i++){
-        document.getElementById(`${i}`).disabled = false;
-
-        //let aye = document.getElementById(`pit${i}`)
-}
-}
-
-function firstPlayerTurn(){
         for(let i=1;i<7;i++){
-                document.getElementById(`${i}`).disabled = false;
-        
+                document.getElementById(`${i}`).style.opacity = "0.1";
+                 document.getElementById(`${i}`).style.pointerEvents = "none"
+
                 //let aye = document.getElementById(`pit${i}`)
         }
         for(let i=8;i<14;i++){
-                document.getElementById(`${i}`).disabled = true;
-        
+                document.getElementById(`${i}`).style.opacity = "1";
+                document.getElementById(`${i}`).style.pointerEvents = "visible";
+
                 //let aye = document.getElementById(`pit${i}`)
         }
 }
+function firstPlayerTurn(){
+        for(let i=1;i<7;i++){
+                document.getElementById(`${i}`).style.opacity = "1";
+                document.getElementById(`${i}`).style.pointerEvents = "visible";
+
+                //let aye = document.getElementById(`pit${i}`)
+        }
+        for(let i=8;i<14;i++){
+                document.getElementById(`${i}`).style.opacity = "0.1";
+                document.getElementById(`${i}`).style.pointerEvents = "none";
+
+                //let aye = document.getElementById(`pit${i}`)
+        }
+}
+
 for(let i = 0; i < arr.length; i++){
         //console.log(i)
         //console.log(arr[i])
-        console.log(pits[i])
+        //console.log(pits[i])
 
         if(i>0&&i<7){
                 pits[i].addEventListener("click", (e)=>{
                         //console.log(arr[i])
-                        //for pits 1 through 9 
+                        //for pits 1 through 6
 
-                        //record the value of the clicked pit
+                        //store the value of the clicked pit in variable count
                         count = parseInt(pits[i].innerHTML)
 
-                        //reset the count of the clicked pit to 0
+                        //reset the count of the clicked pit element to 0
                         arr[i].count = 0
                         pits[i].innerHTML = parseInt(0)
+
+
                         //count = parseInt(pits[i].innerHTML)
                         // console.log(i+count)
                         //if(i+1!==1 && i+1 !==10){
@@ -166,7 +214,7 @@ for(let i = 0; i < arr.length; i++){
                         //add one to each pit from i+1 to i + the pit value+1 to skip the clicked value
                         for(let j=i+1; j<i+(count+1); j++){
                                 //console.log(j)
-                                let  counter = j - (j-1)
+                                let counter = j - (j-1)
                         
                                 if(j<14){
                                         /*if(j==7){
@@ -181,14 +229,14 @@ for(let i = 0; i < arr.length; i++){
                                         pits[j].innerHTML = parseInt(pits[j].innerHTML) + parseInt(counter)
                                         //console.log(lastPitAmount)
                                 }
-                                else if(j==14){
+                                else if(j == 14){
                                         count = count +1
                                 }
                                 else {
                                         let lastPitAmount = pits[j-pits.length].innerHTML
                                         let lastPitClass = pits[j-pits.length].classList.contains("store")
                                         let lastPitId = pits[j-pits.length].id
-                                        //console.log(aye)
+                                        console.log(aye)
                                         lastMoveAmount.push({lastPitClass, lastPitAmount, lastPitId})
                                         pits[j-pits.length].innerHTML = parseInt(pits[j-pits.length].innerHTML) + parseInt(counter)
                                 }
@@ -197,10 +245,7 @@ for(let i = 0; i < arr.length; i++){
                        
                         console.log(lastMoveAmount[lastMoveAmount.length-1])
                         if(lastMoveAmount[lastMoveAmount.length-1].lastPitClass){
-                                for(let i=8;i<14;i++){
-                                        document.getElementById(`${i}`).disabled = true;
-                                        //aye.style.display = "none"
-                                }
+                               firstPlayerTurn()
                         }
                         else if(lastMoveAmount[lastMoveAmount.length-1].lastPitAmount==0&&lastMoveAmount[lastMoveAmount.length-1].lastPitId<7){
                                 console.log(lastMoveAmount[lastMoveAmount.length-1].lastPitId)  
@@ -283,22 +328,13 @@ for(let i = 0; i < arr.length; i++){
                                 stealMove(lastMoveAmount[lastMoveAmount.length-1].lastPitId)
                         } 
                         else {
-                                for(let i=1;i<7;i++){
-                                        document.getElementById(`${i}`).disabled = true;
-
-                                        //let aye = document.getElementById(`pit${i}`)
-                                        }
-                                        for(let i=8;i<14;i++){
-                                                document.getElementById(`${i}`).disabled = false;
-        
-                                                //let aye = document.getElementById(`pit${i}`)
-                                                }
+                               secondPlayerTurn();
                         }              
                 })
+                
         }
-        
 }
-        //for pits 10 through 16
+
 for(let i = 0; i < arr.length; i++){
 
         if(i>7&&i<14){
@@ -349,7 +385,6 @@ for(let i = 0; i < arr.length; i++){
                         if(lastMoveAmount[lastMoveAmount.length-1].lastPitClass){
                                 for(let i=1;i<7;i++){
                                         document.getElementById(`${i}`).disabled = true;
-                                        console.log("stink")
                                         secondPlayerTurn()
                                         //let aye = document.getElementById(`pit${i}`)
                                 }
@@ -424,7 +459,6 @@ for(let i = 0; i < arr.length; i++){
                                         opp.innerHTML= 0
                                         lastMove.innerHTML= 0
                                         firstPlayerTurn()
-
                                         //add the content of pit1 to the stor
                                         //add the
                                 }
@@ -434,19 +468,42 @@ for(let i = 0; i < arr.length; i++){
                                 stealMove(lastMoveAmount[lastMoveAmount.length-1].lastPitId)
                         } 
                         else {
-                                for(let i=1;i<7;i++){
-                                document.getElementById(`${i}`).disabled = false;
-
-                                //let aye = document.getElementById(`pit${i}`)
-                                }
-                                for(let i=8;i<14;i++){
-                                        document.getElementById(`${i}`).disabled = true;
-
-                                        //let aye = document.getElementById(`pit${i}`)
-                                }
+                                firstPlayerTurn();
                         }              
         
                 })
         }
 
 }
+
+function addMarbles(){
+for(let i = 0; i<pits.length;i++){
+        if(pits[i].innerHTML!==0){
+                let count = parseInt(pits[i].innerHTML)
+                pits[i].innerHTML = 4
+
+
+                for(let j = 0; j<count;j++){
+                        let marble = document.createElement("div")
+                        marble.classList.add("marble")
+                        let textMarble = "o"
+                        pits[i].append(marble)
+
+
+                }
+
+
+
+        }
+        else {
+        }
+}
+}
+
+//const app = document.getElementById("app")
+//app.style.display = "none";
+
+
+
+//addMarbles()
+/* function clickHandlers(){}*/
